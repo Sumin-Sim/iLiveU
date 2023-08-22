dbe1cf34aed8f1c70f598c4ea5eba33ab9405dbb
SimSumin <skewer92@gmail.com>
2023-08-22 17:38:10 +0900

    header_footer_setting_0822

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,99 @@
+import { Link } from "react-router-dom";
+
+import "../css/Footer.css";
+
 export default function Footer() {
   return (
-    <>Footer</>
+    <footer>
+      <p>I Live U</p>
+
+      <div>
+        <dl>
+          <dt>고객센터<span>Customer Support</span></dt>
+
+          <dd><Link to="tel:012-345-6789">012-345-6789</Link></dd>
+          <dd>
+            <dfn>운영시간</dfn>
+            오전 09:00 ~ 오후 06:00
+          </dd>
+          <dd>
+            <dfn>점심시간</dfn>
+            오후 12:00 ~ 오후 01:00
+          </dd>
+          <dd>주말 및 공휴일 휴무</dd>
+          <dd><Link to="support">온라인 고객센터</Link></dd>
+        </dl>
+
+        <ul>
+          <li><Link to="">인스타그램</Link></li>
+          <li><Link to="">유튜브</Link></li>
+          <li><Link to="">네이버 블로그</Link></li>
+        </ul>
+      </div>
+
+      <div>
+        <address>
+          <p>회사정보<span>Company Information</span></p>
+
+          <div>
+            <dl>
+              <dt>회사명</dt>
+              <dd>아이리브유&#40;I Live U&#41;</dd>
+            </dl>
+            
+            <dl>
+              <dt>대표자</dt>
+              <dd>홍길동</dd>
+            </dl>
+
+            <dl>
+              <dt>주소</dt>
+              <dd>서울특별시 서울구 서울대로 123&#40;서울로 1가&#41; 4층</dd>
+            </dl>
+          </div>
+
+          <div>
+            <dl>
+              <dt>사업자 등록번호</dt>
+              <dd>012-34-56789</dd>
+            </dl>
+
+            <dl>
+              <dt>통신판매신고번호</dt>
+              <dd>2023-서울서울-6789</dd>
+            </dl>
+
+            <dl>
+              <dt>제품문의</dt>
+              <dd><Link to="tel:012-345-6789">012-345-6789</Link></dd>
+            </dl>
+
+            <dl>
+              <dt>FAX</dt>
+              <dd>012-345-6780</dd>
+            </dl>
+          </div>
+
+          <div>
+            <dl>
+              <dt>이메일 주소</dt>
+              <dd><Link to="mailto:iliveu@iliveu.com">iliveu@iliveu.com</Link></dd>
+            </dl>
+
+            <dl>
+              <dt>제휴문의</dt>
+              <dd><Link to="mailto:iliveu@iliveu.com">iliveu@iliveu.com</Link></dd>
+            </dl>
+            
+            <dl>
+              <dt>개인정보관리책임자</dt>
+              <dd>홍길서&#40;<Link to="mailto:ilivev@iliveu.com">ilivev@iliveu.com</Link>&#41;</dd>
+            </dl>
+          </div>
+        </address>
+
+        <small>Copyright &copy; 2023 아이리브유 All Rights Reserved.</small>
+      </div>
+    </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,76 @@
+import { Link } from "react-router-dom";
+
+import "../css/Header.css";
+
 export default function Header() {
   return (
-    <>Header</>
+    <header>
+      <div className="headerBottom">
+        <h1><Link to="/">I Live U</Link></h1>
+
+        <form>
+          <label htmlFor="search"><Link to="search">검색</Link></label>
+          <input type="text" name="search" id="search" />
+        </form>
+      </div>
+
+      <div className="headerTop">
+        <ul>
+          <li><Link to="aboutUs">About Us</Link></li>
+          <li><Link to="login">로그인</Link></li>
+          <li><Link to="mypage">마이페이지</Link></li>
+          <li><Link to="wishList">장바구니</Link></li>
+          <li><Link to="liked">관심상품</Link></li>
+        </ul>
+      </div>
+
+      <nav>
+        <h2>메인 내비게이션</h2>
+
+        <ul>
+          <li>
+            <Link to="">Tableware</Link>
+            <ul>
+              <li>식기<span>Tableware</span></li>
+              <li><Link to="">공기&amp;접시</Link></li>
+              <li><Link to="">다용도볼</Link></li>
+              <li><Link to="">수저</Link></li>
+              <li><Link to="">컵&amp;잔</Link></li>
+              <li><Link to="">물병</Link></li>
+            </ul>
+          </li>
+
+          <li>
+            <Link to="">Kitchen</Link>
+            <ul>
+              <li>주방<span>Kitchen</span></li>
+              <li><Link to="">조리도구</Link></li>
+              <li><Link to="">주방용품&amp;소품</Link></li>
+            </ul>
+          </li>
+
+          <li>
+            <Link to="">Table Deco</Link>
+            <ul>
+              <li>테이블 장식<span>Table Deco</span></li>
+              <li><Link to="">쟁반&amp;트레이</Link></li>
+              <li><Link to="">식탁보&amp;매트</Link></li>
+              <li><Link to="">코스터</Link></li>
+            </ul>
+          </li>
+
+          <li>
+            <Link to="">ETC</Link>
+            <ul>
+              <li>기타 상품<span>ETC</span></li>
+              <li><Link to="">도시락통</Link></li>
+              <li><Link to="">식판</Link></li>
+            </ul>
+          </li>
+
+          <li><Link to="">On Sale</Link></li>
+        </ul>  
+      </nav>
+    </header>
   );
 }
